6dc5205e28878d1eed9a44e2c0c3e8e53dd5eef8
antonio <antoninho.ferreira2015@gmail.com>
2026-03-03 19:24:46 -0300

    Processamento básico

--- src/main/java/org/project/application/Program.java
+++ src/main/java/org/project/application/Program.java
@@ -1,14 +1,23 @@
 package org.project.application;
 
+import org.project.entities.Company;
 import org.project.services.DataExtractionService;
+import org.project.services.ReadingCadopCsv;
+import org.project.services.WritingCadopService;
 import org.project.utils.CreateDirectoriesUtil;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Program {
     public static void main(String[] args) {
 
+        Map<String, Company> companies = new HashMap<>();
+
         CreateDirectoriesUtil.creatingDirectories("dados_brutos");
         CreateDirectoriesUtil.creatingDirectories("dados_processados");
         DataExtractionService.executeExtraction();
-
+        ReadingCadopCsv.readingCompanyFile("dados_brutos/Relatorio_cadop.csv", companies);
+        WritingCadopService.writeCadop(companies);
     }
 }
---
+++ src/main/java/org/project/services/LogService.java
@@ -0,0 +1,37 @@
+package org.project.services;
+
+import org.project.exceptions.DomainException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class LogService {
+    private String fileName;
+    private String ansRegister;
+    private String description;
+    private String CNPJRegister;
+
+    public LogService() {
+    }
+
+    public LogService(String ansRegister, String cnpjRegister, String description, String fileName) {
+        this.ansRegister = ansRegister;
+        this.CNPJRegister = cnpjRegister;
+        this.description = description;
+        this.fileName = fileName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s, %s, %s", CNPJRegister, ansRegister, fileName, description);
+    }
+
+    public static void logWriter(LogService log) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("registroLog.csv", true))) {
+            bw.write(log.toString());
+            bw.newLine();
+        } catch (DomainException | IOException e) {
+            throw new DomainException("Não foi possível gravar o arquivo de log!");
+        }
+    }
+}
